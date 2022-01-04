46c7fd447c5b11cfaff02926c5ec857f611038be
Antii <coolkid111000111@gmail.com>
2022-01-04 16:52:06 +0000

    fixed the discord rich presence
    me smart

--- source/Discord.hx
+++ source/Discord.hx
@@ -43,8 +43,8 @@ class DiscordClient
 		DiscordRpc.presence({
 			details: "In the Menus",
 			state: null,
-			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageKey: 'bbb',
+			largeImageText: "Funkin' Corner"
 		});
 	}
 
@@ -79,7 +79,7 @@ class DiscordClient
 		DiscordRpc.presence({
 			details: details,
 			state: state,
-			largeImageKey: 'icon',
+			largeImageKey: 'bbb',
 			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
